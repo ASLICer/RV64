9060878c2fb096eac4fa5356bbc214c352d8c9d2
ASLICer <xieqiaohui2022@email.szu.edu.cn>
2024-08-20 07:01:46 +0000

    update rtl/rename.v. 解决了RAW、WAW相关性
    Signed-off-by: ASLICer <xieqiaohui2022@email.szu.edu.cn>

--- rtl/rename.v
+++ rtl/rename.v
@@ -31,10 +31,10 @@ module rename#(
     output [4:0] instr2_prd,
     output [4:0] instr3_prd， 
     //目的寄存器的旧物理寄存器编号
-    output [4:0] instr0_preprd,
-    output [4:0] instr1_preprd,
-    output [4:0] instr2_preprd,
-    output [4:0] instr3_preprd   
+    output reg [4:0] instr0_preprd,
+    output reg [4:0] instr1_preprd,
+    output reg [4:0] instr2_preprd,
+    output reg [4:0] instr3_preprd   
 );
 reg [4:0] rat [31:0]；//重命名映射表
 reg [4:0] freelist [63:0];//空闲列表,FIFO实现
@@ -48,12 +48,22 @@ reg [4:0] instr2_rat_prs1;
 reg [4:0] instr2_rat_prs2;
 reg [4:0] instr3_rat_prs1;
 reg [4:0] instr3_rat_prs2;
+
 always@(posedge clk)begin
     if(rst)
         fl_raddr <= 1'b0;
     else
         fl_raddr <= fl_raddr+4;
 end
+//从RAT中读取目的寄存器对应的的旧物理寄存器编号
+always@(posedge clk)begin
+    instr0_preprd <= rat[instr0_rd];
+    instr1_preprd <= rat[instr1_rd];
+    instr2_preprd <= rat[instr2_rd];
+    instr3_preprd <= rat[instr3_rd];    
+end
+
+//从RAT中读取源寄存器对应的的物理寄存器编号
 always@(posedge clk)begin
     instr0_rat_prs1 <= rat[instr0_rs1];
     instr0_rat_prs2 <= rat[instr0_rs2];
@@ -67,11 +77,31 @@ end
 //从空闲列表中找到空闲的物理寄存器，作为指令的目的寄存器对应的物理寄存器(由于相关性，不一定是正确的)
 //组合逻辑读freelist,因为读完后还要写新的映射关系进RAT(上升沿写入)
 //整个过程在一个周期内完成
+reg [4:0] instr0_fl_prd;
+reg [4:0] instr1_fl_prd;
+reg [4:0] instr2_fl_prd;
+reg [4:0] instr3_fl_prd;
 always@(*)begin
-    instr0_fl_prd <= freelist[fl_raddr];
-    instr1_fl_prd <= freelist[fl_raddr+1];
-    instr2_fl_prd <= freelist[fl_raddr+2];
-    instr3_fl_prd <= freelist[fl_raddr+3];
+    instr0_fl_prd = freelist[fl_raddr];
+    instr1_fl_prd = freelist[fl_raddr+1];
+    instr2_fl_prd = freelist[fl_raddr+2];
+    instr3_fl_prd = freelist[fl_raddr+3];
+end
+//目的寄存器的物理寄存器编号就是从空闲列表找到空闲的物理寄存器编号
+asign instr0_prd = instr0_fl_prd;
+asign instr1_prd = instr1_fl_prd;
+asign instr2_prd = instr2_fl_prd;
+asign instr3_prd = instr3_fl_prd;
+//向RAT中写入新的映射关系(目的寄存器对应的新的物理寄存器,解决WAW相关性)
+always@(posedge clk)begin
+    if((instr0_rd != instr1_rd) && (instr0_rd != instr2_rd) && (instr0_rd != instr3_rd))
+        rat[instr0_rd] <= instr0_fl_prd;
+    if((instr1_rd != instr2_rd) && (instr1_rd != instr3_rd))
+        rat[instr1_rd] <= instr1_fl_prd;
+    if((instr2_rd != instr3_rd))
+        rat[instr2_rd] <= instr2_fl_prd;
+    //指令3的新映射关系一定会被写入
+    rat[instr3_rd] <= instr3_fl_prd;
 end
 //RAW相关性检查(后面指令的源寄存器是否等于前面指令的目的寄存器)
 //第一条指令的源寄存器对应的物理寄存器只能来自RAT
@@ -115,5 +145,6 @@ assign instr3_prs1 = raw3_rs1[2] ? instr2_fl_prd :
                      raw3_rs1[0] ? instr0_fl_prd : instr3_rat_prs1; 
 assign instr3_prs2 = raw3_rs2[2] ? instr2_fl_prd :
                      raw3_rs2[1] ? instr1_fl_prd : 
-                     raw3_rs2[0] ? instr0_fl_prd : instr3_rat_prs2;                                                           
+                     raw3_rs2[0] ? instr0_fl_prd : instr3_rat_prs2;   
+                                                                             
 endmodule
