27d05d9e83ce58a2ef127c7a4ef3590fa6f3355e
ASLICer <xieqiaohui2022@email.szu.edu.cn>
2024-08-22 09:58:20 +0000

    update rtl/arbiter.v.
    Signed-off-by: ASLICer <xieqiaohui2022@email.szu.edu.cn>

--- rtl/arbiter.v
+++ rtl/arbiter.v
@@ -73,33 +73,33 @@ end
 always@(*)begin
     for(i=0;i<8;i=i+1)begin
         case(req16[i*2+1:i*2])
-            2'b00:age8[i] = age[i*2];
-            2'b01:age8[i] = age[i*2];
-            2'b10:age8[i] = age[i*2+1];
-            2'b11:age8[i] = (age[i*2] < age[i*2+1]) ? age[i*2] : age[i*2+1];
+            2'b00:addr8[i] = i*2;
+            2'b01:addr8[i] = i*2;
+            2'b10:addr8[i] = i*2+1;
+            2'b11:addr8[i] = (age[i*2] < age[i*2+1]) ? i*2 : i*2+1;
         endcase
     end 
     for(i=0;i<4;i=i+1)begin
         case(req8[i*2+1:i*2])
-            2'b00:age4[i] = age8[i*2];
-            2'b01:age4[i] = age8[i*2];
-            2'b10:age4[i] = age8[i*2+1];
-            2'b11:age4[i] = (age8[i*2] < age8[i*2+1]) ? age8[i*2] : age8[i*2+1];
+            2'b00:addr4[i] = addr8[i*2];
+            2'b01:addr4[i] = addr8[i*2];
+            2'b10:addr4[i] = addr8[i*2+1];
+            2'b11:addr4[i] = (age8[i*2] < age8[i*2+1]) ? addr8[i*2] : addr8[i*2+1];
         endcase
     end 
     for(i=0;i<2;i=i+1)begin
         case(req4[i*2+1:i*2])
-            2'b00:age2[i] = age4[i*2];
-            2'b01:age2[i] = age4[i*2];
-            2'b10:age2[i] = age4[i*2+1];
-            2'b11:age2[i] = (age4[i*2] < age4[i*2+1]) ? age4[i*2] : age4[i*2+1];
+            2'b00:addr2[i] = addr4[i*2];
+            2'b01:addr2[i] = addr4[i*2];
+            2'b10:addr2[i] = addr4[i*2+1];
+            2'b11:addr2[i] = (age4[i*2] < age4[i*2+1]) ? addr4[i*2] : addr4[i*2+1];
         endcase
     end 
     case(req2[1:0])
-        2'b00:age1 = age2[0];
-        2'b01:age1 = age2[0];
-        2'b10:age1 = age2[1];
-        2'b11:age1 = (age2[0]] < age2[1]) ? age2[0] : age2[1];
+        2'b00:addr1 = addr2[0];
+        2'b01:addr1 = addr2[0];
+        2'b10:addr1 = addr2[1];
+        2'b11:addr1 = (age2[0]] < age2[1]) ? addr2[0] : addr2[1];
     endcase
 end
 endmodule
