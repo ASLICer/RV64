bf69759dd5a363aeda5d4546926487d807d56c48
ASLICer <xieqiaohui2022@email.szu.edu.cn>
2024-08-23 03:41:53 +0000

    update rtl/issue.v.
    Signed-off-by: ASLICer <xieqiaohui2022@email.szu.edu.cn>

--- rtl/issue.v
+++ rtl/issue.v
@@ -47,11 +47,16 @@ wire [4:0] addr_alu0;
 wire [4:0] addr_alu1;
 wire [4:0] addr_mul;
 wire [4:0] addr_ls;
+wire grant_alu0;
+wire grant_alu1;
+wire grant_mul;
+wire grant_ls;
 
 always@(*)begin
     for(i=0;i<16;i=i+1)
         req[i] = (~ciq[PRS1_V] | ciq[PRS1_RDY]) & (~ciq[PRS2_V] | ciq[PRS2_RDY]) & ~ciq[ISSUED];
 end
+//////////////////////仲裁电路//////////////////////////
 arbiter arbiter_u0#(
     .OP(`ALU),
     .OPCODE_WIDTH(7),
@@ -61,6 +66,7 @@ arbiter arbiter_u0#(
     op(op),
     req(req),
     age(age),
+    grant(grant_alu0),
     addr(addr_alu0)
 );//ALU0仲裁器
 arbiter arbiter_u0#(
@@ -74,6 +80,7 @@ arbiter arbiter_u0#(
     op(op),
     req(req),
     age(age),
+    grant(grant_alu1),
     addr(addr_alu1)
 );//ALU1仲裁器
 arbiter arbiter_u0#(
@@ -85,6 +92,7 @@ arbiter arbiter_u0#(
     op(op),
     req(req),
     age(age),
+    grant(grant_mul),
     addr(addr_mul)
 );//MUL仲裁器
 arbiter arbiter_u0#(
@@ -96,8 +104,36 @@ arbiter arbiter_u0#(
     op(op),
     req(req),
     age(age),
+    grant(grant_ls),
     addr(addr_ls)
 );//LOAD_STORE仲裁器
+//////////////////////////////////////////////////////
+///////////////////唤醒电路/////////////////////////
+wire prs1_rdy [15:0];
+wire prs2_rdy [15:0];
+wake_up wake_up_u0#(
+    PRF_WIDTH,
+    PRD_LSB,
+    PRD_MSB,
+    PRS1_LSB,
+    PRS1_MSB,
+    PRS2_LSB,
+    PRS2_MSB
+)(
+    .ciq_prd(ciq[PRD_MSB:PRD_LSB]),
+    .ciq_prs1(ciq[PRS1_MSB:PRS1_LSB]),
+    .ciq_prs1(ciq[PRS2_MSB:PRS2_LSB]),
+    .addr_alu0(addr_alu0),
+    .addr_alu1(addr_alu1),
+    .addr_mul(addr_mul),
+    .addr_ls(addr_ls),
+    .grant_alu0(grant_alu0),
+    .grant_alu1(grant_alu1),
+    .grant_mul(grant_mul),
+    .grant_ls(grant_ls),
 
-
+    .prs1_rdy(),
+    .prs2_rdy()   
+);
+//////////////////////////////////////////////////////
 endmodule
