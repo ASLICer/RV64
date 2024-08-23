0de0a4e4d7aca4d1840bf1b1a1733791df63dfae
ASLICer <xieqiaohui2022@email.szu.edu.cn>
2024-08-23 07:56:24 +0000

    update rtl/issue_queue.v.
    Signed-off-by: ASLICer <xieqiaohui2022@email.szu.edu.cn>

--- rtl/issue_queue.v
+++ rtl/issue_queue.v
@@ -1,4 +1,7 @@
 module issue_queue#(
+    parameter INSTR_NUM = 4,
+    parameter ISSUE_NUM = 4,
+    parameter CIQ_DEPTH = 16,
     parameter OPCODE = 7,
     parameter PRF_WIDTH = 6,
     parameter VALID = 1,
@@ -6,61 +9,57 @@ module issue_queue#(
     parameter ISSUED = 1,
     parameter FREE = 1,
     parameter AGE = 5,
-    parameter IQ_WIDTH = OPCODE+3*PRF_WIDTH+3*VALID+2*READY+ISSUED+FREE+AGE
+    parameter IQ_WIDTH = OPCODE+3*PRF_WIDTH+3*VALID+2*READY+ISSUED+FREE+AGE,
+
+    parameter PRS1_RDY,
+    parameter PRS2_RDY
 )(
     input clk,
-    //指令的opcode
-    input [6:0] instr0_op,
-    input [6:0] instr1_op,
-    input [6:0] instr2_op,
-    input [6:0] instr3_op,
-    //指令是否有源操作数
-    input instr0_prs1_v,
-    input instr0_prs2_v,
-    input instr1_prs1_v,
-    input instr1_prs2_v,
-    input instr2_prs1_v,
-    input instr2_prs2_v,
-    input instr3_prs1_v,
-    input instr3_prs2_v,
-    //指令是否有目的寄存器
-    input instr0_prd_v,
-    input instr1_prd_v,
-    input instr2_prd_v,
-    input instr3_prd_v，   
-    //源寄存器的物理寄存器编号
-    input [PRF_WIDTH-1:0] instr0_prs1,
-    input [PRF_WIDTH-1:0] instr0_prs2,
-    input [PRF_WIDTH-1:0] instr1_prs1,
-    input [PRF_WIDTH-1:0] instr1_prs2,
-    input [PRF_WIDTH-1:0] instr2_prs1,
-    input [PRF_WIDTH-1:0] instr2_prs2,
-    input [PRF_WIDTH-1:0] instr3_prs1,
-    input [PRF_WIDTH-1:0] instr3_prs2,
-    //目的寄存器的物理寄存器编号
-    input [PRF_WIDTH-1:0] instr0_prd,
-    input [PRF_WIDTH-1:0] instr1_prd,
-    input [PRF_WIDTH-1:0] instr2_prd,
-    input [PRF_WIDTH-1:0] instr3_prd,
-    //唤醒电路得到的源寄存器ready信号
-    input prs1_rdy [15:0],
-    input prs2_rdy [15:0],
-    output reg [IQ_WIDTH-1:0] ciq [15:0]//16个表项的集中式发射队列
+    //from rename   
+    input [OPCODE-1:0] instr_op[INSTR_NUM-1:0],//指令的opcode
+    input instr_prs1_v[INSTR_NUM-1:0],//指令是否有来自源寄存器1操作数
+    input instr_prs2_v[INSTR_NUM-1:0],//指令是否有来自源寄存器2操作数  
+    input instr_prd_v[INSTR_NUM-1:0],//指令是否有目的寄存器     
+    input [PRF_WIDTH-1:0] instr_prs1[INSTR_NUM-1:0],//源寄存器1对应的物理寄存器编号
+    input [PRF_WIDTH-1:0] instr_prs2[INSTR_NUM-1:0],//源寄存器2对应的物理寄存器编号  
+    input [PRF_WIDTH-1:0] instr_prd[INSTR_NUM-1:0],//目的寄存器对应的物理寄存器编号
+    input [AGE-1:0] age [INSTR_NUM-1:0];//指令的年龄
+    //from allocation  
+    input [3:0] free_addr [INSTR_NUM-1:0],//发射队列空闲表项的地址  
+    input [INSTR_NUM-1:0] free_valid,//所在地址确实是空闲的
+    //from aribiter
+    input [3:0] arbit_addr [ISSUE_NUM-1:0],//仲裁出的指令所在发射队列地址
+    input [ISSUE_NUM-1:0] arbit_grant;//仲裁出的结果是有效的
+    //from wake up
+    input prs1_rdy [CIQ_DEPTH-1:0],//唤醒电路得到的源寄存器1的ready信号
+    input prs2_rdy [CIQ_DEPTH-1:0],//唤醒电路得到的源寄存器2的ready信号
+    output reg [IQ_WIDTH-1:0] ciq [CIQ_DEPTH-1:0]//16个表项的集中式发射队列
 );
 
 
-//找到4个空闲表项写入指令
+//找到空闲表项将重命名后的指令写入发射队列
 always@(posedge clk)begin
-    if(free0_valid)
-        ciq[free0_addr] <= {instr0_op,instr0_prs1,instr0_prs1_v,instr0_prs1_rdy,instr0_prs2,instr0_prs2_v,instr0_prs2_rdy,instr0_prd,instr0_prd_v,instr0_age,2'b00};  
-    if(free1_valid)  
-        ciq[free1_addr] <= {instr1_op,instr1_prs1,instr1_prs1_v,instr1_prs1_rdy,instr1_prs2,instr1_prs2_v,instr1_prs2_rdy,instr1_prd,instr1_prd_v,instr1_age,2'b00};
-    if(free2_valid)
-        ciq[free2_addr] <= {instr2_op,instr2_prs1,instr2_prs1_v,instr2_prs1_rdy,instr2_prs2,instr2_prs2_v,instr2_prs2_rdy,instr2_prd,instr2_prd_v,instr2_age,2'b00};
-    if(free3_valid)
-        ciq[free3_addr] <= {instr3_op,instr3_prs1,instr3_prs1_v,instr3_prs1_rdy,instr3_prs2,instr3_prs2_v,instr3_prs2_rdy,instr3_prd,instr3_prd_v,instr3_age,2'b00};    
+    for(i=0;i<INSTR_NUM;i=i+1)
+        if(free_valid[i])
+            ciq[free_addr[i]] <= {instr_op[i],instr_prs1[i],instr_prs1_v[i],instr_prs1_rdy[i],instr_prs2[i],instr_prs2_v[i],instr_prs2_rdy[i],instr_prd[i],instr_prd_v[i],instr_age[i],2'b00};  
 end
+
+integer i,j;
+//根据仲裁电路结果更新issued信号
 always@(posedge clk)begin
-    
+    for(i=0;i<CIQ_DEPTH;i=i+1)begin
+        for(j=0;j<ISSUE_NUM;j=j+1)begin
+            if(i == arbit_addr[j] && arbit_grant[j])
+                ciq[i][ISSUED] == 1'b1;//表明该指令已经被选择发射
+        end 
+    end
+end
+//根据唤醒电路结果更新ready信号
+always@(posedge clk)begin
+    for(i=0;i<CIQ_DEPTH;i=i+1)
+        if(ciq[i][0] = 1'b0)begin//该表项不是空闲的
+            ciq[i][PRS1_RDY] = instr_prs1_rdy[i];
+            ciq[i][PRS2_RDY] = instr_prs2_rdy[i];
+        end
 end
 endmoudle
