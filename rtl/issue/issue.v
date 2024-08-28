cd03997a6f12d1f3a6b91354fdc664c4290187fd
ASLICer <xieqiaohui2022@email.szu.edu.cn>
2024-08-28 10:41:34 +0000

    update rtl/issue/issue.v.
    Signed-off-by: ASLICer <xieqiaohui2022@email.szu.edu.cn>

--- rtl/issue/issue.v
+++ rtl/issue/issue.v
@@ -2,14 +2,21 @@ module issue#(
     parameter DECODE_NUM = 4,
     parameter ISSUE_NUM = 4,
     parameter CIQ_DEPTH = 16,
+    parameter DATA_WIDTH = 64,
+
     parameter OPCODE = 7,
+    parameter FUN3 = 3,
+    parameter FUN7 =1,
+    parameter IMME = DATA_WIDTH,
+    
+
     parameter PRF_WIDTH = 6,
     parameter VALID = 1,
     parameter READY = 1,
     parameter ISSUED_WIDTH = 1,
     parameter FREE_WIDTH = 1,
     parameter AGE_WIDTH = 5,
-    parameter IQ_WIDTH = OPCODE+3*PRF_WIDTH+3*VALID+2*READY+ISSUED_WIDTH+FREE_WIDTH+AGE,
+    parameter IQ_WIDTH = OPCODE+FUN3+FUN7+IMME+3*PRF_WIDTH+3*VALID+2*READY+ISSUED_WIDTH+FREE_WIDTH+AGE,
 
     parameter FREE = 0,
     parameter ISSUED = FREE+1,
@@ -25,14 +32,24 @@ module issue#(
     parameter PRS1_RDY = PRS2_MSB+1,
     parameter PRS1_V = PRS1_RDY+1,
     parameter PRS1_LSB = PRS1_V+1,
-    parameter PRS1_MSB = PRS1_LSB+PRF_WIDTH-1,  
-    parameter OP_LSB = PRS1_MSB+1,
+    parameter PRS1_MSB = PRS1_LSB+PRF_WIDTH-1, 
+    parameter IMME_LSB = PRS1_MSB+1,
+    parameter IMME_MSB = IMME_LSB+IMME-1,
+    parameter FUN7_B = IMME_MSB+1,
+    parameter FUN3_LSB = FUN7_B+1,
+    parameter FUN3_MSB = FUN3_LSB+FUN3-1,
+    parameter OP_LSB = FUN3_MSB+1,
     parameter OP_MSB = OP_LSB+OPCODE_WIDTH-1
 
 )(
     input clk,
-    //from rename   
+    //////////指令进入发射队列//////////
+    //from instr_decode  
     input [OPCODE-1:0] instr_op [DECODE_NUM-1:0],//指令的opcode
+    input [FUN3-1:0] instr_func3 [DECODE_NUM-1:0],//指令的func3
+	input [DECODE_NUM-1:0] instr_func7,//指令的func7
+    input [IMME-1:0] instr_imme [DECODE_NUM-1:0],//指令的立即数
+    //from rename 
     input instr_prs1_v [DECODE_NUM-1:0],//指令是否有来自源寄存器1操作数
     input instr_prs2_v [DECODE_NUM-1:0],//指令是否有来自源寄存器2操作数  
     input instr_prd_v [DECODE_NUM-1:0],//指令是否有目的寄存器     
@@ -40,18 +57,63 @@ module issue#(
     input [PRF_WIDTH-1:0] instr_prs2 [DECODE_NUM-1:0],//源寄存器2对应的物理寄存器编号  
     input [PRF_WIDTH-1:0] instr_prd [DECODE_NUM-1:0],//目的寄存器对应的物理寄存器编号
     input [AGE-1:0] age [DECODE_NUM-1:0];//指令的年龄
+
+    //////////被选中的指令离开发射队列//////////
+    //to register file
+    output [PRF_WIDTH-1:0] issue_prs1 [ISSUE_NUM-1:0],//被发射指令的源寄存器1对应的物理寄存器编号   
+    output [PRF_WIDTH-1:0] issue_prs2 [ISSUE_NUM-1:0],//被发射指令的源寄存器2对应的物理寄存器编号  
+    output [PRF_WIDTH-1:0] issue_prd  [ISSUE_NUM-1:0],//被发射指令的目的寄存器对应的物理寄存器编号  
+    //to control
+    output [OPCODE-1:0] issue_op [DECODE_NUM-1:0],//指令的opcode
+    output [FUN3-1:0] issue_func3 [ISSUE_NUM-1:0],//指令的func3
+	output [ISSUE_NUM-1:0] issue_func7,//指令的func7
     //to FU
-    output [DATA_WIDTH-1:0] src0 [DECODE_NUM-1:0],//第一个操作数   
-    output [DATA_WIDTH-1:0] src1 [DECODE_NUM-1:0]//第二个操作数   
+    output [IMME-1:0] issue_imme [ISSUE_NUM-1:0]//指令的立即数
+
 );
 reg [IQ_WIDTH-1:0] ciq [CIQ_DEPTH-1:0];//16个表项的集中式发射队列
-
+integer i,j;
+always@(posedge clk or posedge rst)begin
+    if(rst)begin
+        for(i=0;i<ISSUE_NUM;i=i+1)begin
+            issue_op[i]    <= 1'b0;
+            issue_func3[i] <= 1'b0;
+            issue_func7[i] <= 1'b0;
+            issue_imme[i]  <= 1'b0;
+            issue_prs1[i]  <= 1'b0;
+            issue_prs2[i]  <= 1'b0;
+            issue_prd[i]   <= 1'b0;
+        end
+    end
+    else begin
+        for(i=0;i<ISSUE_NUM;i=i+1)begin
+            if(arbit_grant[i])begin////仲裁出的结果是有效的,否则每周期发射的指令可能少于4条
+                issue_op[i]    <= ciq[arbit_addr[i]][OP_MSB:OP_MSB];
+                issue_func3[i] <= ciq[arbit_addr[i]][FUN3_MSB:FUN3_MSB];
+                issue_func7[i] <= ciq[arbit_addr[i]][FUN7_B];
+                issue_imme[i]  <= ciq[arbit_addr[i]][IMME_MSB:IMME_LSB];
+                issue_prs1[i]  <= ciq[arbit_addr[i]][PRS1_MSB:PRS1_LSB];
+                issue_prs2[i]  <= ciq[arbit_addr[i]][PRS2_MSB:PRS2_LSB];
+                issue_prd[i]   <= ciq[arbit_addr[i]][PRD_MSB:PRD_LSB];   
+            end
+            else begin
+                issue_op[i]    <= 1'b0;
+                issue_func3[i] <= 1'b0;
+                issue_func7[i] <= 1'b0;
+                issue_imme[i]  <= 1'b0;
+                issue_prs1[i]  <= 1'b0;
+                issue_prs2[i]  <= 1'b0;
+                issue_prd[i]   <= 1'b0;
+            end      
+        end
+    end
+end
 //////////////////////分配电路(寻找发射队列空闲表项)//////////////////////////
 reg [CIQ_DEPTH-1:0] ciq_free;//16个空闲标志位
 wire [3:0] free_addr [DECODE_NUM-1:0];//发射队列空闲表项的地址
 wire [DECODE_NUM-1:0] free_valid;//所在地址确实是空闲的 
 
-integer i,j;
+
 always@(*)begin
     for(i=0;i<CIQ_DEPTH;i=i+1)
         ciq_free[i] = ciq[i][0];
@@ -70,7 +132,13 @@ issue_queue #(
     DECODE_NUM,
     ISSUE_NUM,
     CIQ_DEPTH,
+    DATA_WIDTH,
+
     OPCODE,
+    FUN3,
+    FUN7,
+    IMME,
+
     PRF_WIDTH,
     VALID,
     READY,
@@ -83,6 +151,10 @@ issue_queue #(
 )issue_queue_u0(
     clk,   
     instr_op,//指令的opcode 
+    instr_func3,//指令的func3
+    instr_func7,//指令的func7
+    instr_imme,//指令的立即数
+
     instr_prs1_v,//指令是否有来自源寄存器1操作数
     instr_prs2_v,//指令是否有来自源寄存器2操作数   
     instr_prd_v, //指令是否有目的寄存器    
