4f9f3073bbab22c9a2f8c4a864b05cd01639a52a
ASLICer <xieqiaohui2022@email.szu.edu.cn>
2024-08-28 09:02:39 +0000

    update rtl/issue/issue.v. 多周期指令结束muti_finish
    Signed-off-by: ASLICer <xieqiaohui2022@email.szu.edu.cn>

--- rtl/issue/issue.v
+++ rtl/issue/issue.v
@@ -56,17 +56,17 @@ always@(*)begin
     for(i=0;i<CIQ_DEPTH;i=i+1)
         ciq_free[i] = ciq[i][0];
 end
-allocation allocation_u0#(
+allocation #(
     DECODE_NUM,
     DECODE_NUM
-)(
+)allocation_u0(
     ciq_free,
     free_addr,
     free_valid  
 );
 ////////////////////////////////////////////////
 ////////////////////发射队列////////////////////////
-issue_queue issue_queue_u0#(
+issue_queue #(
     DECODE_NUM,
     ISSUE_NUM,
     CIQ_DEPTH,
@@ -80,7 +80,7 @@ issue_queue issue_queue_u0#(
     IQ_WIDTH,
     PRS1_RDY,
     PRS2_RDY
-)(
+)issue_queue_u0(
     clk,   
     instr_op,//指令的opcode 
     instr_prs1_v,//指令是否有来自源寄存器1操作数
@@ -103,6 +103,7 @@ wire [AGE-1:0] age [CIQ_DEPTH-1:0];//发射队列所有表项的年龄
 wire [3:0] arbit_addr [ISSUE_NUM-1:0];//仲裁出的指令所在发射队列地址
 wire [ISSUE_NUM-1:0] arbit_grant;//仲裁出的结果是有效的
 wire [6:0] OP [ISSUE_NUM-1:0];
+wire muti_finish;//多周期指令是否执行完毕
 always@(*)begin
     OP[0] = `ALU;
     OP[1] = `ALU;
@@ -118,17 +119,18 @@ end
 generate
 genvar i;
     for(i=0;i<ISSUE_NUM;i=i+1)begin:arbiter_group
-        arbiter arbiter_block#(
+        arbiter #(
             .OP(OP[i]),
             .OPCODE_WIDTH(7),
             .REQ(1),
             .AGE_WIDTH(5)
-        )(
+        )arbiter_block(
             .op(op),
             .req(req),
             .age(age),
             .grant(arbit_grant[i]),
-            .addr(arbit_addr[i])
+            .addr(arbit_addr[i]),
+            .muti_finish(muti_finish)
         );
     end
 endgenerate
@@ -138,35 +140,33 @@ wire prs1_rdy [CIQ_DEPTH-1:0];//经过唤醒电路得到发射队列所有源寄
 wire prs2_rdy [CIQ_DEPTH-1:0];//经过唤醒电路得到发射队列所有源寄存器2的ready信号
 //from issue_queue
 reg [PRF_WIDTH-1:0] arbit_prd[ISSUE_NUM-1:0];//被arbiter选中的指令的目的寄存器对应的物理寄存器编号
+reg [ISSUE_NUM-1:0] arbit_prd_v;//被arbiter选中的指令是否有目的寄存器
 reg [PRF_WIDTH-1:0] ciq_prs1[CIQ_DEPTH-1:0];//发射队列中所有指令的源寄存器1对应的物理寄存器编号
 reg [PRF_WIDTH-1:0] ciq_prs2[CIQ_DEPTH-1:0];//发射队列中所有指令的源寄存器2对应的物理寄存器编号
 
 always@(*)begin
     for(i=0;i<ISSUE_NUM;i=i+1)begin
         arbit_prd[i] = ciq[arbit_addr[i]][PRD_MSB:PRD_LSB];
+        arbit_prd_v[i] = ciq[arbit_addr[i]][PRDV];
     end
     for(j=0;j<CIQ_DEPTH;j=i+1)begin
         ciq_prs1[j] = ciq[j][PRS1_MSB:PRS1_LSB];
         ciq_prs2[j] = ciq[j][PRS2_MSB:PRS2_LSB];
     end
 end
-wake_up wake_up_u0#(
+wake_up #(
     ISSUE_NUM,
     PRF_WIDTH,
-    CIQ_DEPTH,
-    PRD_LSB,
-    PRD_MSB,
-    PRS1_LSB,
-    PRS1_MSB,
-    PRS2_LSB,
-    PRS2_MSB
-)(
+    CIQ_DEPTH
+)wake_up_u0(
     .arbit_prd(arbit_prd),
+    .arbit_prd_v(arbit_prd_v),
     .ciq_prs1(ciq_prs1),
     .ciq_prs2(ciq_prs2),
     .arbit_grant(arbit_grant),
     .prs1_rdy(prs1_rdy),
-    .prs2_rdy(prs2_rdy)   
+    .prs2_rdy(prs2_rdy),
+    .muti_finish(muti_finish) 
 );
 //////////////////////////////////////////////////////
 endmodule
