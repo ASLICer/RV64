76ca9f21afffb7ad9aba1687ea1165fdb4cb67af
ASLICer <xieqiaohui2022@email.szu.edu.cn>
2024-08-28 07:07:04 +0000

    update rtl/issue/wake_up.v. bus保持不变上次的值可能会误唤醒
    Signed-off-by: ASLICer <xieqiaohui2022@email.szu.edu.cn>

--- rtl/issue/wake_up.v
+++ rtl/issue/wake_up.v
@@ -18,7 +18,8 @@ wire [PRF_WIDTH-1:0] tag_bus [ISSUE_NUM-1:0];
 integer i,j;
 always@(*)begin
     for(i=0;i<ISSUE_NUM;i=i+1)begin
-        tag_bus[i] = (arbit_grant[i] && arbit_prd_v[i]) ? arbit_prd[i] : tag_bus[i];
+        //tag_bus[i] = (arbit_grant[i] && arbit_prd_v[i]) ? arbit_prd[i] : tag_bus[i];//如果本周期bus对应的仲裁电路没有没有选出指令发射，bus置零
+        tag_bus[i] = (arbit_grant[i] && arbit_prd_v[i]) ? arbit_prd[i] : 1'b0;//而不应该保持不变，保持不变可能会误唤醒，而0寄存器一直是准备好的
     end
 end
 
@@ -29,7 +30,7 @@ always@(*)begin
         for(j=0;j<ISSUE_NUM;j=i+1)begin
             if(tag_bus[j] == ciq_prs1[i])
                 prs1_rdy[i] = 1'b1;
-            if(tag_bus[j] == ciq_prs1[i])
+            if(tag_bus[j] == ciq_prs2[i])
                 prs2_rdy[i] = 1'b1;
         end
     end         
