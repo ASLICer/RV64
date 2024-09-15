87ebf669c5afdb86b56fabf707e2bd444a676d44
ASLICer <14315666+aslicer@user.noreply.gitee.com>
2024-09-15 18:43:23 +0800

    rv32-order

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _14452_archive_1.so
-_14452_archive_1.so : archive.39/_14452_archive_1.a
+ARCHIVE_OBJS += _10285_archive_1.so
+_10285_archive_1.so : archive.32/_10285_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_14452_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_10285_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_14452_archive_1.so $@
+	@ln -sf .//../simv.daidir//_10285_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.39/_prev_archive_1.a
+_prev_archive_1.so : archive.32/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
